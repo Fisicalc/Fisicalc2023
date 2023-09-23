4b5bd7fc670976199381afdff86e9248f5eadaea
Jonathan Iha <jonathaniha1@gmail.com>
2023-09-22 21:23:33 -0300

    Firs commit

---
+++ js/style.js
@@ -0,0 +1,14 @@
+function show_hide_formulas()
+{
+    var click = document.getElementsByClassName("formulas");
+alert("s");
+
+    if(click.style.display === "none")
+    {
+        click.style.display = "block";
+    } 
+    else 
+    {
+         click.style.display = "none";
+    }
+}

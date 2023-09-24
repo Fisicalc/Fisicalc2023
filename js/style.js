ba74d0b2d2629e5369fab215b57511830074d7fd
Jonathan Iha <jonathaniha1@gmail.com>
2023-09-24 17:17:08 -0300

    Tamanho do botões

--- js/style.js
+++ js/style.js
@@ -1,17 +1,17 @@
 function show_hide_formulas(event)
 {
-    let d = event.target.nextElementSibling;
+    let formulas = event.target.nextElementSibling;
    
-    if(d.style.display === "none")
+    if(formulas.style.display === "none")
     {
         console.log(Object.entries(event.target))
         console.log(event.target);
-        d.style.display = "block";
+        formulas.style.display = "flex";
     } 
     else
     {
         console.log(Object.entries(event.target))
         console.log(event.target);
-        d.style.display = "none";
+        formulas.style.display = "none";
     }
 }

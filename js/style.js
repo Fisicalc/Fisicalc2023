78b4b3d6f54524523dc4b6f71d146d774463b5c2
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2024-04-25 15:28:24 -0300

    Visual adicionada

--- js/style.js
+++ js/style.js
@@ -1,6 +1,14 @@
 function show_hide_formulas(event)
 {
+    const todasFormulas = document.querySelectorAll("div.formulas")
+
     let formulas = event.target.nextElementSibling;
+    
+    for (let index = 0; index < todasFormulas.length; index++) {
+        if(formulas == todasFormulas[index] || todasFormulas[index].display === "none")
+            continue;
+        todasFormulas[index].style.display = "none";
+    }
    
     if(formulas.style.display === "none")
     {

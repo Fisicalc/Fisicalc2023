412a34c686320cffa8dd88efd7a66dd121466fa2
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2024-05-03 23:21:48 -0300

    Novas fórmulas

--- js/formulas/estatica.mjs
+++ js/formulas/estatica.mjs
@@ -1,3 +1,8 @@
 export const estatica = [
-    
+   
+    {
+        nome: "Momento de uma Força (Torque)",
+        formula: "\\vec{M} = F \\cdot r \\cdot sen \\theta",
+        variaveis: ["\\vec{M}", "F", "r", "sen \\theta" ]
+    }
 ];

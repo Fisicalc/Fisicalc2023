406f7311b80d4c6c743d66df52cd9d23f4432f52
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2025-03-16 17:38:13 -0300

    Menu sanduíche ajustado

--- js/barra_formulas.mjs
+++ js/barra_formulas.mjs
@@ -9,14 +9,34 @@ function criarListaFormulas(){
 
         for(const {nome, formula, variaveis} of formulas[nomeCategoriaFormulas]){
             const btnFormula = criarBtnFormula(nome, formula, variaveis);
-            btnFormula.onclick = () => criarFormulaAreaCalculo(formula, variaveis);
-
+            btnFormula.onclick = () => {
+                criarFormulaAreaCalculo(formula, variaveis);
+                esconderFormulas();
+            }; 
             divFormulas.appendChild(btnFormula);
         }
-
         renderMathInElement(document.body, {output: 'html'})
     }
 }
+
+function pesquisarFormulas(){
+    for(const nomeCategoriaFormulas in formulas){
+        const divFormulas = document.querySelector(`.${nomeCategoriaFormulas}`);
+
+        for(const {nome, formula, variaveis} of formulas[nomeCategoriaFormulas]){
+            const btnFormula = criarBtnFormula(nome, formula, variaveis);
+            btnFormula.onclick = () => {
+                criarFormulaAreaCalculo(formula, variaveis);
+                esconderFormulas();
+            };
+            btnFormula.addClass('.botaoFormulaPesquisa');
+            const formulas = querySelectorAll('.botaoFormulaPesquisa');
+            divFormulas.appendChild(btnFormula);
+        }
+        renderMathInElement(document.body, {output: 'html'})
+    }
+}
+
 
 function criarBtnFormula(nome, formula, variaveis){
     const btnFormula = document.createElement("button");
@@ -36,3 +56,13 @@ function criarBtnFormula(nome, formula, variaveis){
 
     return btnFormula;
 }
+
+
+function esconderFormulas(){
+    const menuToggle = document.getElementById("menuToggle");
+    const menuLateral = document.getElementById("barraLateralBotoes");
+    menuToggle.classList.toggle('deactive');
+    menuLateral.classList.toggle('deactive');
+   
+}
+
--- js/style.js
+++ js/style.js
@@ -1,3 +1,7 @@
+
+const menuToggle = document.getElementById("menuToggle");
+const menuLateral = document.getElementById("barraLateralBotoes");
+
 document.querySelectorAll("button.btnCategorias")
     .forEach(btn => btn.addEventListener("click", (event) => show_hide_formulas(event)));
 
@@ -9,33 +13,7 @@ function show_hide_formulas(event)
     divsFormulas.forEach(div => div === divFormulas && !div.classList.contains("visivel") ? div.classList.add("visivel") : div.classList.remove("visivel"))
 }
 
-const menuToggle = document.getElementById("menuToggle");
-
-const menuLateral = document.getElementById("barraLateralBotoes");
-
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('deactive');
     menuLateral.classList.toggle('deactive');
 })
-
-
-//COLOCADO NO SCRIPT QUE define erro
-
-//const erro = document.getElementById("erro");
-// function MensagemErro(texto)
-// {
-//     let mensagem = "ERRO " + texto;
-//     erro.textContent = texto;
-//     erro.classList.remove("fadeOut", "fade-in");
-
-//     void erro.offsetWidth;
-
-//     erro.addEventListener("animationend", function handlerStart(){
-//         erro.removeEventListener("animationend", handlerStart);
-
-//         setTimeout(() => {
-//             erro.classList.remove("fade-in");
-//             erro.classList.add('fadeOut');
-//         }, 300);       
-//     }, {once: true});
-// }
